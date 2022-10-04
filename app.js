f4d32d1d3040784b1f31840be190eac7e1d91d90
Destiny Boone <destaj.db@gmail.com>
2022-10-04 16:57:12 -0400

    DOM manipulation

--- app.js
+++ app.js
@@ -829,3 +829,367 @@ comments
 // }
 
 // console.log('Global Scope: ', a, b, c);
+
+//  SECTION 3 DOM MANIPULATION AND EVENTS
+// let val;
+
+// val = document;
+// val = document.all;
+// val = document.all[0];
+// val = document.all[1];
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[1];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList;
+
+// val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
+
+// let scripts = document.scripts;
+
+// let scriptsArr = Array.from(scripts);
+
+// scriptsArr.forEach(function(script){
+//     console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
+
+// DOM SELECTORS
+
+// // document.getElementById()
+// console.log(document.getElementById('task-title'));
+
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// const taskTitle = document.getElementById('task-title');
+
+// // Change styling
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// //taskTitle.style.display = 'none';
+
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+// // document.querySelector() - only works for ONE element
+
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
+
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc'; // these two wont work because query selector only works for one element
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+
+// document.getElementsByClassName
+
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[0].textContent = 'Hello';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// document.getElementsByTagName
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Hello';
+
+// // Convert HTML Collection into array
+// lis = Array.from(lis);
+// lis.reverse();
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent = `${index}: Hello`;
+// });
+// console.log(lis);
+
+// document.querySelectorAll
+// const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// console.log(items);
+// items.forEach(function(item, index){
+//         console.log(item.className);
+//         item.textContent = `${index}: Hello`;
+// });
+
+// const liOdd = document.querySelectorAll('li:nth-child(odd');
+// const liEven = document.querySelectorAll('li:nth-child(even');
+
+// liOdd.forEach(function(li){
+//        li.style.background = '#ccc';
+// });
+
+// for(let i = 0; i < liEven.length; i++){
+//     liEven[i].style.background = '#f4f4f4';
+// }
+
+// TRAVERSING THE DOM - moving up and down the DOM
+// let val;
+
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelector('li.collection-item:first-child');
+
+// val = listItem;
+// val = list;
+
+// // Get child nodes - node list but includes text nodes and kinds of stuff
+// val = list.childNodes;
+// val = list.childNodes[0];
+// val = list.childNodes[0].nodeName;
+// val = list.childNodes[1].nodeType;
+
+// // Node Type Number Meanings
+// // 1 - Element
+// // 2 - Attribute (deprecated)
+// // 3 - Text Node
+// // 8 - Comment
+// // 9 - Document itself
+// // 10 - Document itself
+
+
+// // Get children element nodes - doesn't include text nodes but returns as html collection
+// val = list.children; //we use children more often
+// val = list.children[1];
+// val = list.children[1].textContent = 'Hello';
+// // Children of children
+// val = list.children[3].children[0].id = 'test-link';
+// val = list.children[3].children[0];
+
+// // first child
+// val = list.firstChild;
+// val = list.firstElementChild; // use this more often
+
+// // Last child
+// val = list.lastChild;
+// val = list.lastElementChild; // use this more often
+
+// // Count child elements
+// val = list.childElementCount;
+
+// // Get parent node
+// val = listItem.parentNode;
+// val = listItem.parentElement;
+// val = listItem.parentElement.parentElement;
+
+// // Get next sibling
+// val = listItem.nextSibling;
+// val = listItem.nextElementSibling; // use this more often
+// val = listItem.nextElementSibling.nextElementSibling;
+
+// // Get prev sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling; // use this more often
+
+// console.log(val);
+
+// CREATING ELEMENTS
+
+// Create element
+// const li = document.createElement('li');
+
+// // Add class
+// li.className = 'collection-item';
+
+// // Add id
+// li.id = 'new-item';
+
+// // Add attribute
+// li.setAttribute('title', 'New Item');
+
+// // Create text node and append
+// li.appendChild(document.createTextNode('Hello World')); // inserts text inside the li item
+
+// // Create new link element
+// const link = document.createElement('a');
+// //Add classes
+// link.className = 'delete-item secondary-content';
+// // Add icon html
+// link.innerHTML = '<i class="fa fa-remove"></i>';
+// // Append link into li
+// li.appendChild(link);
+
+// // Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+// console.log(li);
+
+// REMOVING AND REPLACING ELEMENTS
+
+// Replace Element
+
+// // Create Element
+// const newHeading = document.createElement('h2');
+// // Add id
+// newHeading.id = 'task-title';
+// // New text node
+// newHeading.appendChild(document.createTextNode('Task List'));
+
+// // Get the old heading
+// const oldHeading = document.getElementById('task-title');
+// // Parent
+// const cardAction = document.querySelector('.card-action');
+
+// // Replace
+// cardAction.replaceChild(newHeading, oldHeading);
+
+// // REMOVE ELEMENT
+// const lis = document.querySelectorAll('li');
+// const list = document.querySelector('ul');
+
+// // Remove list item
+// lis[0].remove();
+
+// // Remove child element
+// list.removeChild(lis[3]);
+
+// // CLASSES AND ATTR
+// const firstLi = document.querySelector('li:first-child');
+// const link = firstLi.children[0];
+
+// let val;
+
+// // Classes
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// val = link.classList.add('test');
+// val = link.classList.remove('test');
+// val = link;
+
+// // Attributes
+// val = link.getAttribute('href');
+// val = link.setAttribute('href', 'http://google.com');
+// val = link.hasAttribute('href');
+// link.setAttribute('title', 'Google')
+// val = link.hasAttribute('title');
+// val = link.removeAttribute('title');
+
+// console.log(val);
+
+// EVENT LISTENERS AND THE EVENT OBJECT
+// document.querySelector('.clear-tasks').addEventListener('click',
+// function(e){
+//     console.log('Hello World');
+//     //e.preventDefault();
+// });
+
+// document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+// function onClick(e) {
+//     //console.log('Clicked');
+
+//     let val;
+
+//     val = e;
+
+//     // Event target element
+//     val = e.target;
+//     val = e.target.id;
+//     val = e.target.className;
+//     val = e.target.classList;
+
+//     //e.target.innerText = 'Hello';
+
+//     // Event Type
+//     val = e.type;
+
+//     // Timestamp
+//     val = e.timeStamp;
+
+//     // Coordinates event relative o the window
+//     val = e.clientY;
+//     val = e.clientX;
+
+//     // Cordinates event relative to the element
+//     val = e.offsetY;
+//     val = e.offsetX;
+
+//     console.log(val);
+// }
+
+// // MOUSE EVENTS
+// const clearBtn = document.querySelector('.clear-tasks');
+// const card = document.querySelector('.card');
+// const heading = document.querySelector('h5');
+
+// // Click
+// clearBtn.addEventListener('click', runEvent);
+
+// // Doubleclick
+// clearBtn.addEventListener('dblclick', runEvent);
+
+// // Mouse Down - click and hold
+// clearBtn.addEventListener('mousedown', runEvent);
+
+// // Mouse Up - when release mouse
+// clearBtn.addEventListener('mouseup', runEvent);
+
+// // Mouse Enter - drag mouse into button, only when you enter the parent element completely 
+// clearBtn.addEventListener('mouseenter', runEvent);
+
+// // Mouse Leave - drag mouse out of button, only when you leave the parent element completely 
+// clearBtn.addEventListener('mouseleave', runEvent);
+
+// // Mouse Over - when you go to the parent element from another element inside of the parent element
+// clearBtn.addEventListener('mouseover', runEvent);
+
+// // Mouse Out - when you go to another element inside of the parent element
+// clearBtn.addEventListener('mouseout', runEvent);
+
+// // Mousemove - any movement inside of an element
+// clearBtn.addEventListener('mousemove', runEvent);
+
+// // Event Handler
+// function runEvent(e){
+//     console.log(`EVENT TYPE: ${e.type}`);
+
+//     heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+
+//     document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 4)`;
+// }
+
+// KEYBOARD AND INPUT EVENTS
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+
+form.addEventListener('submit', runEvent);
+
+function runEvent(e){
+    console.log(`EVENT TYPE: ${e.type}`);
+}
+
