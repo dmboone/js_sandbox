0c30cfa410abf00eda4388a090a9320a4819edc6
Destiny Boone <destaj.db@gmail.com>
2022-10-04 09:54:26 -0400

    window object and variable scope

--- app.js
+++ app.js
@@ -758,18 +758,74 @@ comments
 //     console.log('NO');
 // }
 
-let val;
+// let val;
 
-// Outer height and width
-val = window.outerHeight;
-val = window.outerWidth;
+// // Outer height and width
+// val = window.outerHeight;
+// val = window.outerWidth;
 
-// Inner height and width - inside the scrollbars
-val = window.innerHeight;
-val = window.innerWidth;
+// // Inner height and width - inside the scrollbars
+// val = window.innerHeight;
+// val = window.innerWidth;
 
-// Scroll Points
+// // Scroll Points - good for when you want to time animations to a certain scroll point
+// val = window.scrollY;
+// val = window.scrollX;
 
+// // Location Object
+// val = window.location;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.href;
+// val = window.location.search;
 
-console.log(val);
+// Redirect
+// window.location.href = "http://google.com";
 
+// Reload
+//window.location.reload();
+
+// History Object
+// window.history.go(0);
+// val = window.history.length;
+
+// // Navigator Object - deals with the browser
+// val = window.navigator;
+// val = window.navigator.appName;
+// val = window.navigator.appVersion;
+// val = window.navigator.userAgent;
+// val = window.navigator.platform;
+// val = window.navigator.vendor;
+// val = window.navigator.language;
+
+// console.log(val);
+
+// BLOCK SCOPE
+
+// Global Scope
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// function test(){
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('Function Scope: ', a, b, c);
+// }
+
+//test();
+
+// if(true){
+//     // Block Scope
+//     var a = 4; // this actually changes the global variable! DON'T USE VAR
+//     let b = 5; // let and const have a block level scope so better to use
+//     const c = 6;
+//     console.log('If Scope: ', a, b, c);
+// }
+
+// for(let a = 0; a < 10; a++){ // let will not change the global variable
+//     console.log(`Loop: ${a}`);
+// }
+
+// console.log('Global Scope: ', a, b, c);
