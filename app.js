33b5d9d1856b49ea525b4497ac4eef775a7e44ca
Destiny Boone <destaj.db@gmail.com>
2022-10-11 12:07:06 -0400

    arrow functions

--- app.js
+++ app.js
@@ -1317,3 +1317,48 @@ comments
 //     console.log(task);
 // });
 
+// ARROW FUNCTIONS (es6)
+
+// How we've been doing functions
+// const sayHello = function(){
+//     console.log('Hello');
+// }
+
+// Arrow function
+// const sayHello = () => {
+//     console.log('Hello');
+// }
+
+// One line function does not need braces
+// const sayHello = () => console.log('Hello');
+
+// One line return
+// const sayHello = () => 'Hello'; // returns Hello
+// console.log(sayHello());
+
+// Return object
+// const sayHello = () => ({msg: 'Hello'});
+// console.log(sayHello());
+
+// Single param does not need parenthesis
+// const sayHello = (name) => console.log(`Hello ${name}`);
+// sayHello('Destiny');
+
+// Multiple params need parenthesis
+// const sayHello = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
+// sayHello('Destiny', 'Boone');
+
+// const users = ['Nathan', 'John', 'William'];
+// const nameLengths  = users.map(function(name){
+//     return name.length;
+// });
+
+// Shorter
+// const nameLengths  = users.map((name) => {
+//     return name.length;
+// });
+
+// Shortest
+// const nameLengths = users.map(name => name.length);
+
+// console.log(nameLengths);
