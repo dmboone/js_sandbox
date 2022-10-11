a8cebe36e954a3eefee0f7429e66bf87a9ee3182
Destiny Boone <destaj.db@gmail.com>
2022-10-11 14:44:45 -0400

    async and await

--- app.js
+++ app.js
@@ -1362,3 +1362,39 @@ comments
 // const nameLengths = users.map(name => name.length);
 
 // console.log(nameLengths);
+
+// ASYNC & AWAIT (es7)
+// async function myFunc(){ // putting async in front automatically creates a Promise
+//     const promise = new Promise((resolve, reject)=>{
+//         setTimeout(() => resolve('Hello'), 1000);
+//     });
+
+//     const error = false; // toggle for error
+
+//     if(!error){
+//         const res = await promise; // Wait until promise is resolved
+//         return res;
+//     } else{
+//         await Promise.reject(new Error('Something went wrong'));
+//     }
+
+    
+// }
+
+// myFunc()
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+// 
+// async function getUsers(){
+//     // await response of the fetch call
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+//     // only proceed once its resolved
+//     const data = await response.json();
+
+//     // only proceed once second promise is resolved
+//     return data;
+// }
+
+// getUsers().then(users => console.log(users));
