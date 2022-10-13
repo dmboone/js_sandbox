5ae88451454db624c98d3f2aedc0a05a97724bee
Destiny Boone <destaj.db@gmail.com>
2022-10-13 16:59:57 -0400

    destructuring

--- app.js
+++ app.js
@@ -1627,3 +1627,58 @@ comments
 // console.log(JSON.stringify({key: 'prop'}));
 // console.log(JSON.stringify({[Symbol('sym1')]: 'prop'}));
 
+// DESTRUCTURING
+// let a, b;
+// [a, b] = [100, 200];
+// // Rest Pattern
+// [a, b, c, ...rest] = [100, 200, 300, 400, 500];
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(rest);
+
+// ({a, b} = {a: 100, b: 200, c: 300, d: 400, 3: 500});
+// ({a, b, ...rest} = {a: 100, b: 200, c: 300, d: 400, 3: 500});
+
+// console.log(a,b);
+// console.log(rest);
+
+// Array Destructuring
+// const people = ['John', 'Beth', 'Mike'];
+// const [person1, person2, person3] = people;
+// console.log(person1, person2, person3);
+
+// Parse array returned from function
+// function getPeople(){
+//     return ['John', 'Beth', 'Mike'];
+// }
+
+// let person1, person2, person3;
+// [person1, person2, person3] = getPeople()
+// console.log(person1, person2, person3);
+
+// OBJECT DESTRUCTURING
+// const person = {
+//     name: 'John Doe',
+//     age: 32,
+//     city: 'Miami',
+//     gender: 'Male',
+//     sayHello: function(){
+//         console.log('Hello');
+//     }
+// }
+
+// Old ES5
+// const name = person.name,
+//     age = person.age,
+//     city = person.city;
+
+// New ES6 Destructuring
+// const { name, age, city } = person;
+// const { name, age, city, sayHello } = person;
+
+// console.log(name, age, city);
+
+// sayHello();
